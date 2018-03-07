6705552a3d18ee4dd761ba1d2b4cc094a0be40bd
Dean Bailey <dbailey@bloodmagic.com>
2018-03-07 15:19:32 -0800

    PIA port forward tuning. Style changes to start and updatePort.sh
    * Retry the PIA port forward if it fails the first time. 3 retrys allowed.
    * Set a connection timeout to 10 seconds and maximum connection time to 60 seconds.
    * Block for port forward attempt and EXIT if fails.
    * BASH 3+ style changes. Minor optimizations.

--- transmission/start.sh
+++ transmission/start.sh
@@ -1,48 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 
 # Source our persisted env variables from container startup
 . /etc/transmission/environment-variables.sh
 
 # This script will be called with tun/tap device name as parameter 1, and local IP as parameter 4
 # See https://openvpn.net/index.php/open-source/documentation/manuals/65-openvpn-20x-manpage.html (--up cmd)
-echo "Up script executed with $*"
-if [ "$4" = "" ]; then
-   echo "ERROR, unable to obtain tunnel address"
-   echo "killing $PPID"
-   kill -9 $PPID
-   exit 1
+echo "Up script executed with ${*}"
+if [[ -z "${4}" ]]; then
+  echo "ERROR, unable to obtain tunnel address"
+  echo "killing ${PPID}"
+  kill -9 "${PPID}"
+  exit 1
 fi
 
 # If transmission-pre-start.sh exists, run it
-if [ -x /scripts/transmission-pre-start.sh ]
-then
-   echo "Executing /scripts/transmission-pre-start.sh"
-   /scripts/transmission-pre-start.sh
-   echo "/scripts/transmission-pre-start.sh returned $?"
+if [[ -x "/scripts/transmission-pre-start.sh" ]]; then
+  echo "Executing /scripts/transmission-pre-start.sh"
+  /scripts/transmission-pre-start.sh
+  echo "/scripts/transmission-pre-start.sh returned ${?}"
 fi
 
-echo "Updating TRANSMISSION_BIND_ADDRESS_IPV4 to the ip of $1 : $4"
-export TRANSMISSION_BIND_ADDRESS_IPV4=$4
+echo "Updating TRANSMISSION_BIND_ADDRESS_IPV4 to the ip of ${1} : ${4}"
+export TRANSMISSION_BIND_ADDRESS_IPV4="${4}"
 
-if [ "combustion" = "$TRANSMISSION_WEB_UI" ]; then
-  echo "Using Combustion UI, overriding TRANSMISSION_WEB_HOME"
-  export TRANSMISSION_WEB_HOME=/opt/transmission-ui/combustion-release
-fi
-
-if [ "kettu" = "$TRANSMISSION_WEB_UI" ]; then
-  echo "Using Kettu UI, overriding TRANSMISSION_WEB_HOME"
-  export TRANSMISSION_WEB_HOME=/opt/transmission-ui/kettu
-fi
+case "${TRANSMISSION_WEB_UI,,}" in
+  combustion)
+    echo "Using Combustion UI, overriding TRANSMISSION_WEB_HOME"
+    export TRANSMISSION_WEB_HOME="/opt/transmission-ui/combustion-release"
+    ;;
+  kettu)
+    echo "Using Kettu UI, overriding TRANSMISSION_WEB_HOME"
+    export TRANSMISSION_WEB_HOME="/opt/transmission-ui/kettu"
+    ;;
+esac
 
 echo "Generating transmission settings.json from env variables"
 # Ensure TRANSMISSION_HOME is created
-mkdir -p ${TRANSMISSION_HOME}
-dockerize -template /etc/transmission/settings.tmpl:${TRANSMISSION_HOME}/settings.json
+mkdir -p "${TRANSMISSION_HOME}"
+dockerize -template "/etc/transmission/settings.tmpl:${TRANSMISSION_HOME}/settings.json"
 
-echo "sed'ing True to true"
-sed -i 's/True/true/g' ${TRANSMISSION_HOME}/settings.json
+echo "sed'ing True to true, False to false (and all variants there of)"
+sed -ri 's/true|false/\L&/ig' "${TRANSMISSION_HOME}/settings.json"
 
-if [ ! -e "/dev/random" ]; then
+if [[ ! -e "/dev/random" ]]; then
   # Avoid "Fatal: no entropy gathering module detected" error
   echo "INFO: /dev/random not found - symlink to /dev/urandom"
   ln -s /dev/urandom /dev/random
@@ -50,28 +50,34 @@ fi
 
 . /etc/transmission/userSetup.sh
 
-if [ "true" = "$DROP_DEFAULT_ROUTE" ]; then
+if [[ "${DROP_DEFAULT_ROUTE,,}" == "true" ]]; then
   echo "DROPPING DEFAULT ROUTE"
-  ip r del default || exit 1
+  _IP_RESP="$(ip r del default 2>&1)"
+  if (( ${?} != 0 )); then
+    echo "ERROR: DROPPING DEFAULT ROUTE FAILED!"
+    echo "${_IP_RESP}"
+    echo "EXITING!"
+    exit 1
+  fi
 fi
 
 echo "STARTING TRANSMISSION"
 exec su --preserve-environment ${RUN_AS} -s /bin/bash -c "/usr/bin/transmission-daemon -g ${TRANSMISSION_HOME} --logfile ${TRANSMISSION_HOME}/transmission.log" &
 
-if [ "$OPENVPN_PROVIDER" = "PIA" ]
-then
-    echo "CONFIGURING PORT FORWARDING"
-    exec /etc/transmission/updatePort.sh &
-else
-    echo "NO PORT UPDATER FOR THIS PROVIDER"
+if [[ "${OPENVPN_PROVIDER}" = "PIA" ]]; then
+  echo "CONFIGURING PORT FORWARDING"
+  /etc/transmission/updatePort.sh
+  if (( ${?} != 0 )); then
+    echo "ERROR: Port forward failed! Exiting!"
+    exit 1
+  fi
 fi
 
 # If transmission-post-start.sh exists, run it
-if [ -x /scripts/transmission-post-start.sh ]
-then
-   echo "Executing /scripts/transmission-post-start.sh"
-   /scripts/transmission-post-start.sh
-   echo "/scripts/transmission-post-start.sh returned $?"
+if [[ -x "/scripts/transmission-post-start.sh" ]]; then
+  echo "Executing /scripts/transmission-post-start.sh"
+  /scripts/transmission-post-start.sh
+  echo "/scripts/transmission-post-start.sh returned ${?}"
 fi
 
 echo "Transmission startup script complete."
